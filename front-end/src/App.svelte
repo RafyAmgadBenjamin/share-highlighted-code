2000ca87ca687eb4bee1bb4607f2bbe194783b49
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-08-29 12:24:58 +0200

    Handle the navigation and the basic structure of the Home page

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -1,11 +1,49 @@
 <script>
-	export let name;
+	import Navigation from './Navigation.svelte';
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<div>
+	<Navigation />
+</div>
 
-<h1>Hello {name}!</h1>
+<!--[Container]-->
+<div class="container">
+	<!--[Title]-->
+	<div class="mt-3">
+		<h1>Sharable highlited code</h1>
+	</div>
+	<div class="row">
+		<!--[Left-Side]-->
+		<div class="col-sm-6">
+			<!--[Content]-->
+			<div class="row">
+				<div class="col-sm-12">
+					<textarea rows="20" cols="50">
+						At w3schools.com you will learn how to make a website. We offer free
+						tutorials in all web development technologies.
+					</textarea>
+				</div>
+			</div>
+			<!--[BTNs]-->
+			<div class="row">
+				<!--[Submit-BTN]-->
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-dark">Submit</button>
+				</div>
+				<!--[Download-BTN]-->
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-dark">Download</button>
+				</div>
+				<!--[Submit-Copy]-->
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-dark">Copy</button>
+				</div>
+			</div>
+		</div>
+		<!--[Right-Side]-->
+		<div class="col-sm-6">
+			<p />
+		</div>
+	</div>
+
+</div>
