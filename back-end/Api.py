7ad67e63bea74dc384d654ceefa50862b02d89f3
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-08-29 17:36:52 +0200

    Working on the back-end, i have added the bottle server and working on the post API

---
+++ back-end/Api.py
@@ -0,0 +1,85 @@
+from bottle import route, run, error, template, abort
+from random import randint
+import random
+import redis
+import json
+from bottle import hook, request, response
+from bottle import post, get, put, delete
+
+#  configuration information
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+
+####
+_allow_origin = '*'
+_allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
+_allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+
+@hook('after_request')
+def enable_cors():
+    '''Add headers to enable CORS'''
+
+    response.headers['Access-Control-Allow-Origin'] = _allow_origin
+    response.headers['Access-Control-Allow-Methods'] = _allow_methods
+    response.headers['Access-Control-Allow-Headers'] = _allow_headers
+
+
+@route('/', method='OPTIONS')
+@route('/<path:path>', method='OPTIONS')
+@post('/api/code/add-highlited-code')
+def add_Highlighted_code():
+    """
+    Add the highlighted code to the Redis database
+    """
+    generatedVal = str(generate_random_no())
+    #add to redis data base
+    print(response.body())
+   # add_url_redis(tinyUrl=generatedVal, originalUrl=originalUrl)
+    #response.headers['Content-Type'] = 'application/json'
+    #response.headers['Cache-Control'] = 'no-cache'
+    #return json.dumps({'tinyUrl': generatedVal})
+
+# # API: used to get the original URL from the tiny URL and redirect to the original URL
+# @route('/<tinyUrl>')
+# def get_original_url(tinyUrl):
+#     r = redis.StrictRedis(host=redis_host, port=redis_port,
+#                           password=redis_password, decode_responses=True)
+#     if r.get(tinyUrl) is not None:
+#         response.status = 303
+#         response.set_header('Location', r.get(tinyUrl))
+#     else:
+#         response.status = 404
+#         abort(404, 'object already exists with that name')  # raise an 404 error
+
+# # Handle the 404 error response
+# @error(404)
+# def error404(error):
+#     return template('views/404.tpl', e=response.status_code)
+
+
+def generate_random_no():
+
+    random.seed(a=None)
+    return randint(0, 1000000)  # randint is inclusive at both ends
+
+
+def add_highlighted_code(radndomNo, highlightedCode):
+    try:
+
+        # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
+        # using the default encoding utf-8.  This is client specific.
+        r = redis.StrictRedis(host=redis_host, port=redis_port,
+                              password=redis_password, decode_responses=True)
+
+        # step 4: Set the data in Redis
+        r.set(radndomNo, highlightedCode)
+        print(r.get(tinyUrl))
+
+        # step 5: Retrieve the data message from Redis
+    except Exception as e:
+        print(e)
+
+
+run(host='localhost', port=8080, debug=True)
